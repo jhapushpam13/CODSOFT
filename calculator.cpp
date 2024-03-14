399078002b9b97f68ac868f9201dc564e723c446
Pushpam Jha <pushpamjha13102004@gmail.com>
2024-03-14 22:46:13 +0530

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,46 @@
+ #include <iostream>
+
+using namespace std;
+
+int main() {
+    char operation;
+    double num1, num2;
+
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    cout << "Enter operation (+, -, *, /): ";
+    cin >> operation;
+
+    double result;
+
+    switch(operation) {
+        case '+':
+            result = num1 + num2;
+            cout << "Result: " << result << endl;
+            break;
+        case '-':
+            result = num1 - num2;
+            cout << "Result: " << result << endl;
+            break;
+        case '*':
+            result = num1 * num2;
+            cout << "Result: " << result << endl;
+            break;
+        case '/':
+            if(num2 != 0) {
+                result = num1 / num2;
+                cout << "Result: " << result << endl;
+            } else {
+                cout << "Error: Division by zero" << endl;
+            }
+            break;
+        default:
+            cout << "Invalid operation" << endl;
+    }
+
+    return 0;
+}
